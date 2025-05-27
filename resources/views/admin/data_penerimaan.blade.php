6bccb78dd4516a2add0aca76f24514d59fcbaca7
gatotpur <yulius.gtt@gmail.com>
2025-05-27 10:48:34 +0700

    add cetak rekap function

--- resources/views/admin/data_penerimaan.blade.php
+++ resources/views/admin/data_penerimaan.blade.php
@@ -347,6 +347,43 @@
                 for (const [key, value] of form.entries()) {
                     params.append(key, value);
                 }
+
+                const fullUrl = `${url}?${params.toString()}`;
+                const request = new Request(
+                    fullUrl, {
+                        method: "GET",
+                        headers: {
+                            'X-CSRF-TOKEN': csrfToken,
+                            'Accept': 'application/pdf'
+                        }
+                    });
+
+                fetch(request)
+                    .then(res => res.blob())
+                    .then(blob => {
+                        const url = URL.createObjectURL(blob);
+                        window.open(url, '_blank');
+                        successAlert('Sukses, Rekap terbuka pada tab baru');
+                    })
+                    .catch(error => {
+                        if (error.status === 422) {
+                            const errors = error.error || error.errors;
+                            errorAlert(error.message);
+                            if (errors) {
+                                processErrors(errors)
+                            }
+                        } else {
+                            const errorMessages = {
+                                401: 'Sesi anda sudah habis 🙏 <br>Silahkan muat ulang halaman untuk melanjutkan! <br> jika masalah masih terjadi silahkan login kembali!',
+                                403: 'Anda tidak memiliki izin untuk mengakses halaman ini 😖',
+                                404: 'Halaman yang dituju tidak ditemukan 🧐',
+                                405: 'Metode tidak valid 🧐 <br>silahkan muat ulang halaman dan coba lagi!',
+                                419: 'Sesi anda sudah habis 🙏 <br>Silahkan muat ulang halaman untuk melanjutkan! <br> jika masalah masih terjadi silahkan login kembali!',
+                                429: 'Terlalu banyak permintaan akses <br>silahkan tunggu beberapa saat 🙏',
+                            };
+                            errorAlert(errorMessages[error.status] || "Terjadi kesalahan, silahkan coba memuat ulang halaman");
+                        }
+                    });
             });
         });
 
