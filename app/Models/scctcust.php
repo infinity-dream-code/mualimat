36e2692885c2bef19fcaeb1cd393ebbf55a475d8
gatotpur <yulius.gtt@gmail.com>
2025-12-22 11:06:10 +0700

    add showVA functions

--- app/Models/scctcust.php
+++ app/Models/scctcust.php
@@ -7,11 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class scctcust extends Model
 {
-    protected $table = 'scctcust';
+    protected $table = "scctcust";
 
-    protected $primaryKey = 'CUSTID';
+    protected $primaryKey = "CUSTID";
 
     public $timestamps = false;
 
     public $incrementing = false;
+
+    public static function showVAMTS($nis): string
+    {
+        $prefix = "751023";
+        $nova = str_pad($nis, 10, "0", STR_PAD_LEFT);
+        return "$prefix$nova";
+    }
+
+    public static function showVAMA($nis): string
+    {
+        $prefix = "797763";
+        $nova = str_pad($nis, 10, "0", STR_PAD_LEFT);
+        return "$prefix$nova";
+    }
 }
