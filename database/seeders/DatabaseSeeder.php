acd8dc8eaffe6489810b09d93c245b579b4e7cec
gatotpur <yulius.gtt@gmail.com>
2026-01-28 09:07:07 +0700

    add default seeder with spatie role and persmission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //make permission
+
+        //untuk membuka dashboard
+        Permission::create(['name' => 'admin-read']);
+        Permission::create(['name' => 'admin-write']);
+        //untuk membuka dashboard admin cms
+        //make role
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $adminRole->givePermissionTo('admin-read');
+        $adminRole->givePermissionTo('admin-write');
+
+        $superAdminRole = Role::firstOrCreate(['name' => 'super-admin']);
+
+        $user = User::firstOrCreate(['username' => 'admin',], [
+            'name' => 'Example admin user',
+            'email' => 'admin@sikeu.com',
+            'password' => bcrypt('admkeu90')
+        ]);
+
+        $user->assignRole($adminRole);
+
+        $user = User::firstOrCreate(['username' => 'super_admin',], [
+            'name' => 'Example admin user',
+            'email' => 'super_admin@sikeu.com',
+            'password' => bcrypt('admkeu90')
+        ]);
+
+        $user->assignRole($superAdminRole);
     }
 }
