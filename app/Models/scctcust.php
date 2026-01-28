85441a33855b8a74ee97932e7e7386568c4611e6
gatotpur <yulius.gtt@gmail.com>
2026-01-28 09:31:52 +0700

    fix missing functions

--- app/Models/scctcust.php
+++ app/Models/scctcust.php
@@ -15,4 +15,18 @@ class scctcust extends Model
     public $timestamps = false;
 
     public $incrementing = false;
+
+    public static function showVAMTS($nis): string
+    {
+        $prefix = "751023";
+        $nova = str_pad($nis, 10, "0", STR_PAD_LEFT);
+        return "$prefix$nova";
+    }
+
+    public static function showVAMA($nis): string
+    {
+        $prefix = "797763";
+        $nova = str_pad($nis, 10, "0", STR_PAD_LEFT);
+        return "$prefix$nova";
+    }
 }
