aec4034732e5d6c72c4097977914d7f9bc436778
gatotpur <yulius.gtt@gmail.com>
2026-01-28 09:07:37 +0700

    create permissions and roles migrations, update users table schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username', 100)->unique();
+            $table->string('name', 100);
+            $table->string('email', 150)->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->dateTime('last_login')->nullable();
             $table->string('password');
+            $table->string('token')->nullable();
+            $table->string('login_token')->nullable();
+            $table->boolean('is_active')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
