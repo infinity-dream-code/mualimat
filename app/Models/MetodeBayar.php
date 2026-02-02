691ed7fcdf7048096d43a24a31585d16cb41f002
gatotpur <yulius.gtt@gmail.com>
2026-02-02 16:24:13 +0700

    update MetodeBayar model to fix filter logic for NULL and empty values

--- app/Models/MetodeBayar.php
+++ app/Models/MetodeBayar.php
@@ -24,8 +24,8 @@ class MetodeBayar extends Model
             '4' => 'H2H VA BMI - EDC',
             '5' => 'H2H VA BMI - MOBILE',
             '6' => 'ALL BMI',
-            null => 'Nomor VA',
-            '' => 'Nomor VA'
+            "NULL" => 'Nomor VA', //NULL not working on default filter logic
+            'empty' => 'Nomor VA' //'' not working on default filter logic
         ];
     }
 }
