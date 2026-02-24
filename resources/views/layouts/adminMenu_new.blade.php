2ee890391f6999e939cb3a40f6b8baa2cb6c3560
gatotpur <yulius.gtt@gmail.com>
2026-02-24 11:01:47 +0700

    update admin menu: replace "Rekap Penerimaan Harian" menu icon with updated design for consistency

--- resources/views/layouts/adminMenu_new.blade.php
+++ resources/views/layouts/adminMenu_new.blade.php
@@ -93,14 +93,7 @@
         </li>
         <li class="menu-item  {{ Request::is(['admin/rekap-penerimaan-harian'])  ? 'active' : '' }}">
             <a href="{{route('admin.rekap-penerimaan-harian.index')}}" class="menu-link">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                     class="menu-icon icon icon-tabler icons-tabler-outline icon-tabler-file-check">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M14 3v4a1 1 0 0 0 1 1h4"/>
-                    <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2"/>
-                    <path d="M9 15l2 2l4 -4"/>
-                </svg>
+                <i class="menu-icon ri-receipt-line"></i>
                 <div data-i18n="Rekap Penerimaan Harian">Rekap Penerimaan Harian</div>
             </a>
         </li>
