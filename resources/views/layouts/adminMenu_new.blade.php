a8498ddb6defd851c66d5f5a9260b8454aa5a5bd
tsabitgholib <tsabitgholib@gmail.com>
2026-07-28 20:48:19 +0700

    p

--- resources/views/layouts/adminMenu_new.blade.php
+++ resources/views/layouts/adminMenu_new.blade.php
@@ -1,23 +1,17 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
         <a href="{{route('admin.index')}}" class="app-brand-link">
-              <span class="app-brand-logo demo">
+            <span class="app-brand-logo demo">
                 <span style="color: var(--bs-primary)">
-                  <img width="50" height="50" src="{{asset('logo.png')}}" alt="logo">
+                    <img width="50" height="50" src="{{asset('logo.png')}}" alt="logo">
                 </span>
-              </span>
+            </span>
             <span class="app-brand-text demo menu-text fw-bold ms-2">SIKEU</span>
         </a>
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
             <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M11.4854 4.88844C11.0081 4.41121 10.2344 4.41121 9.75715 4.88844L4.51028 10.1353C4.03297 10.6126 4.03297 11.3865 4.51028 11.8638L9.75715 17.1107C10.2344 17.5879 11.0081 17.5879 11.4854 17.1107C11.9626 16.6334 11.9626 15.8597 11.4854 15.3824L7.96672 11.8638C7.48942 11.3865 7.48942 10.6126 7.96672 10.1353L11.4854 6.61667C11.9626 6.13943 11.9626 5.36568 11.4854 4.88844Z"
-                    fill="currentColor"
-                    fill-opacity="0.6"/>
-                <path
-                    d="M15.8683 4.88844L10.6214 10.1353C10.1441 10.6126 10.1441 11.3865 10.6214 11.8638L15.8683 17.1107C16.3455 17.5879 17.1192 17.5879 17.5965 17.1107C18.0737 16.6334 18.0737 15.8597 17.5965 15.3824L14.0778 11.8638C13.6005 11.3865 13.6005 10.6126 14.0778 10.1353L17.5965 6.61667C18.0737 6.13943 18.0737 5.36568 17.5965 4.88844C17.1192 4.41121 16.3455 4.41121 15.8683 4.88844Z"
-                    fill="currentColor"
-                    fill-opacity="0.38"/>
+                <path d="M11.4854 4.88844C11.0081 4.41121 10.2344 4.41121 9.75715 4.88844L4.51028 10.1353C4.03297 10.6126 4.03297 11.3865 4.51028 11.8638L9.75715 17.1107C10.2344 17.5879 11.0081 17.5879 11.4854 17.1107C11.9626 16.6334 11.9626 15.8597 11.4854 15.3824L7.96672 11.8638C7.48942 11.3865 7.48942 10.6126 7.96672 10.1353L11.4854 6.61667C11.9626 6.13943 11.9626 5.36568 11.4854 4.88844Z" fill="currentColor" fill-opacity="0.6"/>
+                <path d="M15.8683 4.88844L10.6214 10.1353C10.1441 10.6126 10.1441 11.3865 10.6214 11.8638L15.8683 17.1107C16.3455 17.5879 17.1192 17.5879 17.5965 17.1107C18.0737 16.6334 18.0737 15.8597 17.5965 15.3824L14.0778 11.8638C13.6005 11.3865 13.6005 10.6126 14.0778 10.1353L17.5965 6.61667C18.0737 6.13943 18.0737 5.36568 17.5965 4.88844C17.1192 4.41121 16.3455 4.41121 15.8683 4.88844Z" fill="currentColor" fill-opacity="0.38"/>
             </svg>
         </a>
     </div>
@@ -25,12 +19,13 @@
     <div class="menu-inner-shadow"></div>
 
     <ul class="menu-inner py-1">
-        <li class="menu-item  {{ Request::is(['admin'])  ? 'active' : '' }}">
+        <li class="menu-item {{ Request::is(['admin']) ? 'active' : '' }}">
             <a href="{{route('admin.index')}}" class="menu-link">
                 <i class="menu-icon ri ri-home-3-line"></i>
                 <div data-i18n="Beranda">Beranda</div>
             </a>
         </li>
+
         <li class="menu-item {{ Request::is(['admin/master-data*']) ? 'active open' : '' }}">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon ri ri-database-2-line"></i>
@@ -79,6 +74,7 @@
                 </li>
             </ul>
         </li>
+
         <li class="menu-item {{ Request::is(['admin/keuangan*']) ? 'active open' : '' }}">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon ri ri-wallet-3-line"></i>
@@ -163,6 +159,7 @@
                 </li>
             </ul>
         </li>
+
         <li class="menu-item {{ Request::is(['admin/manual-input*']) ? 'active open' : '' }}">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon ri ri-edit-2-line"></i>
@@ -176,6 +173,7 @@
                 </li>
             </ul>
         </li>
+
         <li class="menu-item {{ Request::is(['admin/rekap-data*']) ? 'active open' : '' }}">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon ri ri-file-list-3-line"></i>
@@ -194,6 +192,7 @@
                 </li>
             </ul>
         </li>
+
         <li class="menu-item {{ Request::is([
             'admin/data-tagihan',
             'admin/data-penerimaan',
@@ -202,7 +201,9 @@
             'admin/rekap-penerimaan-harian',
             'admin/rekap-cek-pelunasan',
             'admin/rekap-saldo',
+            'admin/rekap-saldo/*',
             'admin/rekap-saldo-saku',
+            'admin/rekap-saldo-saku/*',
             'admin/cek-pelunasan',
             'admin/potongan-tagihan*',
         ]) ? 'active open' : '' }}">
@@ -211,47 +212,47 @@
                 <div data-i18n="Laporan">Laporan</div>
             </a>
             <ul class="menu-sub">
-                <li class="menu-item {{ Request::is(['admin/data-tagihan']) ? 'active' : '' }}">
+                <li class="menu-item {{ Request::is('admin/data-tagihan') ? 'active' : '' }}">
                     <a href="{{route('admin.data-tagihan.index')}}" class="menu-link">
                         <div data-i18n="Data Tagihan">Data Tagihan</div>
                     </a>
                 </li>
-                <li class="menu-item {{ Request::is(['admin/data-penerimaan']) ? 'active' : '' }}">
+                <li class="menu-item {{ Request::is('admin/data-penerimaan') ? 'active' : '' }}">
                     <a href="{{route('admin.data-penerimaan.index')}}" class="menu-link">
                         <div data-i18n="Data Penerimaan">Data Penerimaan</div>
                     </a>
                 </li>
-                <li class="menu-item {{ Request::is(['admin/rekap-tagihan']) ? 'active' : '' }}">
+                <li class="menu-item {{ Request::is('admin/rekap-tagihan') ? 'active' : '' }}">
                     <a href="{{route('admin.rekap-tagihan.index')}}" class="menu-link">
                         <div data-i18n="Rekap Tagihan">Rekap Tagihan</div>
                     </a>
                 </li>
-                <li class="menu-item {{ Request::is(['admin/rekap-penerimaan']) ? 'active' : '' }}">
+                <li class="menu-item {{ Request::is('admin/rekap-penerimaan') ? 'active' : '' }}">
                     <a href="{{route('admin.rekap-penerimaan.index')}}" class="menu-link">
                         <div data-i18n="Rekap Penerimaan">Rekap Penerimaan</div>
                     </a>
                 </li>
-                <li class="menu-item {{ Request::is(['admin/rekap-penerimaan-harian']) ? 'active' : '' }}">
+                <li class="menu-item {{ Request::is('admin/rekap-penerimaan-harian') ? 'active' : '' }}">
                     <a href="{{route('admin.rekap-penerimaan-harian.index')}}" class="menu-link">
                         <div data-i18n="Rekap Penerimaan Harian">Rekap Penerimaan Harian</div>
                     </a>
                 </li>
-                <li class="menu-item {{ Request::is(['admin/cek-pelunasan']) ? 'active' : '' }}">
+                <li class="menu-item {{ Request::is('admin/cek-pelunasan') ? 'active' : '' }}">
                     <a href="{{route('admin.cek-pelunasan.index')}}" class="menu-link">
                         <div data-i18n="Cek Pelunasan">Cek Pelunasan</div>
                     </a>
                 </li>
-                <li class="menu-item {{ Request::is(['admin/rekap-cek-pelunasan']) ? 'active' : '' }}">
+                <li class="menu-item {{ Request::is('admin/rekap-cek-pelunasan') ? 'active' : '' }}">
                     <a href="{{route('admin.rekap-cek-pelunasan.index')}}" class="menu-link">
                         <div data-i18n="Rekap Cek Pelunasan">Rekap Cek Pelunasan</div>
                     </a>
                 </li>
-                <li class="menu-item {{ Request::is(['admin/rekap-saldo']) ? 'active' : '' }}">
+                <li class="menu-item {{ Request::is('admin/rekap-saldo') || Request::is('admin/rekap-saldo/*') ? 'active' : '' }}">
                     <a href="{{route('admin.rekap-saldo.index')}}" class="menu-link">
-                        <div data-i18n="Rekap Saldo">Rekap Saldo</div>
+                        <div data-i18n="Rekap Saldo VA">Rekap Saldo VA</div>
                     </a>
                 </li>
-                <li class="menu-item {{ Request::is(['admin/rekap-saldo-saku']) ? 'active' : '' }}">
+                <li class="menu-item {{ Request::is('admin/rekap-saldo-saku') || Request::is('admin/rekap-saldo-saku/*') ? 'active' : '' }}">
                     <a href="{{route('admin.rekap-saldo-saku.index')}}" class="menu-link">
                         <div data-i18n="Rekap Saldo Saku">Rekap Saldo Saku</div>
                     </a>
@@ -296,12 +297,9 @@
         </li>
 
         <li class="menu-item mt-auto pb-2">
-            <a href="{{route('logout')}}" class="menu-link btn-danger text-white"  onclick="event.preventDefault();
-                              document.getElementById('logout-form').submit();">
+            <a href="{{route('logout')}}" class="menu-link btn-danger text-white" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 <i class="menu-icon ri ri-logout-box-r-line"></i>
-                <div data-i18n="Logout">
-                    Logout
-                </div>
+                <div data-i18n="Logout">Logout</div>
             </a>
         </li>
     </ul>
